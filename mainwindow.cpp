1077a5f8a603409e657f9c590165574079e4f061
zhangxiang <2570175014@qq.com>
2026-01-05 15:15:03 +0800

    å®play  mode

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <QPixmap>
 #include <QPalette>
 #include <QBrush>
+#include <QMessageBox>
+#include <QRandomGenerator>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -29,22 +31,61 @@ MainWindow::MainWindow(QWidget *parent)
     // 设置音频源
     m_player->setSource(QUrl::fromLocalFile("/Users/xiang/Desktop/music/Fa_Ru_Xue.mp3"));
 
-
+    // 关联播放按钮
     connect(ui->playPauseBtn, &QPushButton::clicked, this, &MainWindow::handlePlaySlot);
-    // connect(player, &QMediaPlayer::playbackStateChanged,
-    //         this, [=](QMediaPlayer::PlaybackState state){
-    //             switch (state) {
-    //             case QMediaPlayer::PlayingState:
-    //                 qDebug() << "Playing";
-    //                 break;
-    //             case QMediaPlayer::PausedState:
-    //                 qDebug() << "Paused";
-    //                 break;
-    //             case QMediaPlayer::StoppedState:
-    //                 qDebug() << "Stopped";
-    //                 break;
-    //             }
-    //         });
+
+    QString musicDir = "/Users/xiang/Desktop/music";
+    loadAppointMusicDir(musicDir);
+
+    // 默认隐藏MusicList列表
+    ui->rightPanel->hide();
+    // 设置音乐列表背景透明
+    ui->musicList->setAttribute(Qt::WA_TranslucentBackground);
+    ui->musicList->setStyleSheet("background: transparent;");
+
+    // 初始化动画
+    m_listAnim = new QPropertyAnimation(ui->rightPanel, "geometry");
+    m_listAnim->setDuration(300);
+    m_listAnim->setEasingCurve(QEasingCurve::OutCubic);
+
+    // 关联音乐列表按钮
+    connect(ui->musicListBtn, &QPushButton::clicked, this, [=](){
+        QRect startRect, endRect;
+
+        int w = ui->rightPanel->width();
+        int h = ui->centralwidget->height();
+
+        if (!m_listVisible) {
+            // 从右侧外面滑入
+            startRect = QRect(width(), 0, w, h);
+            endRect   = QRect(width() - w, 0, w, h);
+            ui->rightPanel->show();
+        } else {
+            // 滑出到右侧
+            startRect = QRect(width() - w, 0, w, h);
+            endRect   = QRect(width(), 0, w, h);
+        }
+
+        m_listAnim->stop();
+        m_listAnim->setStartValue(startRect);
+        m_listAnim->setEndValue(endRect);
+        m_listAnim->start();
+
+        m_listVisible = !m_listVisible;
+    });
+
+    // 关联歌曲列表中表项
+    connect(ui->musicList, &QListWidget::itemClicked, this, &MainWindow::onMusicItemClicked);
+
+    // 当前歌曲播放结束自动播放下一曲（默认顺序播放）
+    connect(m_player, &QMediaPlayer::mediaStatusChanged, this, [this](QMediaPlayer::MediaStatus status) {
+        if (status == QMediaPlayer::EndOfMedia) {
+            playNext();
+        }
+    });
+
+    // 关联随机播放按钮
+    connect(ui->modeBtn, &QPushButton::clicked, this, &MainWindow::onModeBtnClicked);
 
 }
 
@@ -61,6 +102,31 @@ void MainWindow::handlePlaySlot()
     } else {
         m_player->play();
         ui->playPauseBtn->setIcon(QIcon(":/play.png"));
+    }
+}
+
+void MainWindow::onMusicItemClicked(QListWidgetItem *item)
+{
+    if (!item)
+        return;
+
+
+    int index = ui->musicList->row(item);
+
+    if (index < 0 || index >= m_musicList.size())
+        return;
+
+    playMusicByIndex(index);
+}
+
+void MainWindow::onModeBtnClicked()
+{
+    if (m_playMode == PlayMode::Sequential) {
+        m_playMode = PlayMode::Random;
+        ui->modeBtn->setIcon(QIcon(":/shuffle.png"));
+    } else {
+        m_playMode = PlayMode::Sequential;
+        ui->modeBtn->setIcon(QIcon(":/order.png"));
     }
 }
 
@@ -94,8 +160,146 @@ void MainWindow::setButtonStyle(QPushButton *btn,const QString& iconPath)
 void MainWindow::initButton()
 {
     setButtonStyle(ui->prevBtn, ":/previous.png");
-    setButtonStyle(ui->playPauseBtn, ":/play.png");
+    setButtonStyle(ui->playPauseBtn, ":/pause.png");
     setButtonStyle(ui->nextBtn, ":/next.png");
     setButtonStyle(ui->modeBtn, ":/order.png");
     setButtonStyle(ui->musicListBtn, ":/musicList.png");
 }
+
+void MainWindow::loadAppointMusicDir(const QString &dirPath)
+{
+    ui->musicList->clear();
+
+    QDir dir(dirPath);
+    if (!dir.exists()) {
+        QMessageBox::warning(this, "waring", "Current Dir isn't exist");
+        return ;
+    }
+
+    QStringList filters = {"*.mp3", "*.wav", "*.m4a"};
+    QFileInfoList files = dir.entryInfoList(filters, QDir::Files);
+
+    for (QFileInfo &info : files) {
+        // 显示给用户看的：歌名
+        QListWidgetItem *item = new QListWidgetItem(info.baseName());
+        // 把路径保存到成员变量中
+        m_musicList.append(info.absoluteFilePath());
+        ui->musicList->addItem(item);
+    }
+}
+
+void MainWindow::playMusicByIndex(int index)
+{
+    if (index < 0 || index >= m_musicList.size())
+        return;
+
+    m_currentIndex = index;
+    m_currentMusicPath = m_musicList[index];
+
+    m_player->setSource(QUrl::fromLocalFile(m_currentMusicPath));
+    m_player->play();
+
+    updatePlayingItemState(index);
+    recordHistory(index);       // 记录播放历史
+    ui->playPauseBtn->setIcon(QIcon(":/play.png"));
+
+}
+
+void MainWindow::updatePlayingItemState(int newIndex)
+{
+    // 清除旧状态
+    for (int i = 0; i < ui->musicList->count(); ++i) {
+        QListWidgetItem *item = ui->musicList->item(i);
+        item->setForeground(Qt::white);
+        item->setFont(QFont());
+        item->setIcon(QIcon());
+    }
+
+    // 设置当前播放项
+    QListWidgetItem *currentItem = ui->musicList->item(newIndex);
+
+    QFont font = currentItem->font();
+    font.setBold(true); // 粗体
+    currentItem->setFont(font);
+
+    currentItem->setForeground(QColor(0, 200, 255)); // 播放中颜色
+    currentItem->setIcon(QIcon(":/playing.png"));
+
+    ui->musicList->setCurrentRow(newIndex);
+}
+
+int MainWindow::getNextIndex()
+{
+    if (m_musicList.isEmpty())
+        return -1;
+
+    if (m_playMode == PlayMode::Sequential) {
+        return (m_currentIndex + 1) % m_musicList.size();
+    }
+
+    // Random
+    if (m_musicList.size() == 1)
+        return m_currentIndex;
+
+    int next;
+    do {
+        next = QRandomGenerator::global()->bounded(m_musicList.size());
+    } while (next == m_currentIndex);
+
+    return next;
+}
+
+void MainWindow::playNext()
+{
+    if (m_musicList.isEmpty())
+        return;
+
+    // 获取下一首歌曲下标
+    int nextIndex = getNextIndex();
+    if (nextIndex != -1)
+        playMusicByIndex(nextIndex);
+}
+
+void MainWindow::recordHistory(int index)
+{
+    // 如果从中间跳转，截断后面的历史
+    if (m_historyPos + 1 < m_playHistory.size()) {
+        m_playHistory.resize(m_historyPos + 1);
+    }
+
+    m_playHistory.append(index);
+    m_historyPos = m_playHistory.size() - 1;
+}
+
+void MainWindow::playPrev()
+{
+    if (m_musicList.isEmpty())
+        return;
+
+    // 顺序播放：直接 -1
+    if (m_playMode == PlayMode::Sequential) {
+        int prev = (m_currentIndex - 1 + m_musicList.size()) % m_musicList.size();
+        playMusicByIndex(prev);
+        return;
+    }
+
+    // 随机播放：从历史回退
+    // 随机模式的上一曲不是随机歌曲，而是刚刚听过的上一曲
+    if (m_historyPos > 0) {
+        m_historyPos--;
+        int prevIndex = m_playHistory[m_historyPos];
+        playMusicByIndex(prevIndex);
+    }
+}
+
+void MainWindow::on_prevBtn_clicked()
+{
+    playPrev();
+}
+
+
+void MainWindow::on_nextBtn_clicked()
+{
+    playNext();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -5,8 +5,18 @@
 #include <QPushButton>
 #include <QMediaPlayer>
 #include <QAudioOutput>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QPropertyAnimation>
+#include <QListWidget>
 
 QT_BEGIN_NAMESPACE
+enum class PlayMode {
+    Sequential,   // 顺序播放
+    Random        // 随机播放
+};
+
 namespace Ui {
 class MainWindow;
 }
@@ -22,6 +32,12 @@ public:
 
 public slots:
     void handlePlaySlot();
+    void onMusicItemClicked(QListWidgetItem *item);
+    void onModeBtnClicked();
+
+private slots:
+    void on_prevBtn_clicked();
+    void on_nextBtn_clicked();
 
 private:
     // 设置背景
@@ -30,10 +46,38 @@ private:
     void setButtonStyle(QPushButton* btn, const QString& filename);
     // 初始化按钮
     void initButton();
+    // 加载指定目录下的歌曲信息
+    void loadAppointMusicDir(const QString & dirPath);
+    // 播放指定路径歌曲
+    void playMusicByIndex(int idx);
+    // 更新音乐列表中当前播放状态
+    void updatePlayingItemState(int newIndex);
+    // 获取下一首歌曲(下标)
+    int getNextIndex();
+    // 播放下一首（shuffle/order）
+    void playNext();
+    // 记录历史播放记录
+    void recordHistory(int index);
+    // 播放上一首（shuffle/order）
+    void playPrev();
 
-private:
+
+private:   
     Ui::MainWindow *ui;
-    QMediaPlayer* m_player;
+    // 音乐播放部件
+    QMediaPlayer *m_player;
     QAudioOutput *m_audioOutput;
+    // 动画显示部件
+    QPropertyAnimation *m_listAnim;
+    bool m_listVisible = false;
+    // 播放歌曲路径
+    QStringList m_musicList;
+    int m_currentIndex = -1;
+    QString m_currentMusicPath;
+    // 播放模式
+    PlayMode m_playMode = PlayMode::Sequential;
+    // 播放历史记录
+    QVector<int> m_playHistory;
+    int m_historyPos = -1;
 };
 #endif // MAINWINDOW_H
